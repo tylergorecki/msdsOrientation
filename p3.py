110c61a4aa5823d2ee5ed31d8aa5539b0953736f
Tyler Gorecki <ttg6nx@virginia.edu>
2024-06-14 15:08:39 -0400

    complete p3

--- p3.py
+++ p3.py
@@ -2,13 +2,13 @@
 
 maxPrime = 1
 divisor = 2
-num = 16 # 13195 #600851475143
+num = 38 # 13195 #600851475143
 
 while num > 1:
     while num % divisor == 0:
         # print(divisor)
         maxPrime = divisor
-        num //= divisor
+        num /= divisor
     divisor += 1
 
 print(maxPrime)
