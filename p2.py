0c67f2a80ba42b304ba761fa6dc771e5b84c0981
Tyler Gorecki <ttg6nx@virginia.edu>
2024-06-14 13:54:24 -0400

    completed p2

--- p2.py
+++ p2.py
@@ -1,2 +1,17 @@
 # problem 2
 
+first = 1
+next = 2
+sum = 0
+
+while(next < 4000000):
+    #print(first)
+    if next % 2 == 0:
+        sum += next
+        #print(sum)
+    temp = next
+    next += first
+    first = temp
+    #print(next)
+
+print(sum)
