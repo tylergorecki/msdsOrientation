12c337e175cde146b62b2b7db9baad404b21fec7
Tyler Gorecki <ttg6nx@virginia.edu>
2024-06-15 14:17:09 -0400

    complete R problems

--- eulerProblems.R
+++ eulerProblems.R
@@ -4,17 +4,48 @@
 
 sum = 0
 
-for i in 1:1000:
-  if i % 3 == 0 or i % 5 == 0:
-  sum += i
+for (i in 1:999) {
+  if (i %% 3 == 0 || i %% 5 == 0) {
+    sum <- sum + i
+  }
+}
   
 print(sum)
 
 # PROBLEM 2
 
+first = 1
+second = 2
+sum = 0
 
+while (second < 4000000) {
+  #print(first)
+  if (second %% 2 == 0) {
+    sum = sum + second
+  }
+  #print(sum)
+  temp = second
+  second = second + first
+  first = temp
+  #print(next)
+}
+
+print(sum)
 
 # PROBLEM 3
 
+maxPrime = 1
+divisor = 2
+num = 600851475143
 
+while (num > 1) {
+  while (num %% divisor == 0) {
+    # print(divisor)
+    maxPrime = divisor
+    num = num / divisor
+  }
+  divisor = divisor + 1
+}
+
+print(maxPrime)
 
